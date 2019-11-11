a677eee7fe75e73fa6b1e92d62333d6fe55bf3ed
Alunos Unifamma <alunos@intranet.lan>
2019-11-11 15:12:28 -0300

    estou testando

---
+++ src/com/db1/db1start/App.java
@@ -0,0 +1,9 @@
+package com.db1.db1start;
+
+public class App {
+    public static void main(String[] args){
+        /* Metodo main é necessario para que o Java consiga entender */
+
+        System.out.println("Hello world");
+    }
+}
