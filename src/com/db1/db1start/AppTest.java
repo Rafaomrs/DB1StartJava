23ce2977a8359599ee718f95854982d020fb6515
Alunos Unifamma <alunos@intranet.lan>
2019-11-13 16:57:58 -0300

    Revisao dos exercicios em sala de aula

--- src/com/db1/db1start/AppTest.java
+++ src/com/db1/db1start/AppTest.java
@@ -4,57 +4,149 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class AppTest {
-
+    Application application = new Application();
     //Classe para teste unitátio.
     @Test
     public void deveRetornar5(){
-        Application application = new Application();
         Integer expected = 5;
         Integer response = application.soma(2,3);
         Assert.assertEquals(expected, response);
     }
     @Test
     public void deveRetornar8(){
-        Application application = new Application();
         Integer expected = 8;
         Integer response = application.soma(5,3);
         Assert.assertEquals(expected, response);
     }
     @Test
     public void deveRetornar10(){
-        Application application = new Application();
         Integer expected = 10;
         Integer response = application.multiplicacao(5,2);
         Assert.assertEquals(expected, response);
     }
     @Test
     public void deveRetornarSeNumeroEPar(){
-        Application application = new Application();
         Boolean expected = true;
         Boolean response = application.par(10);
         Assert.assertEquals(expected, response);
     }
     @Test
     public void deveSerMaior(){
-        Application application = new Application();
         Integer expected = 10;
         Integer response = application.maior(10,2);
         Assert.assertEquals(expected, response);
     }
     @Test
     public void deveTer5Impar(){
-        Application application = new Application();
         Integer expected = 5;
         Integer response = application.quantidadeImpar(10);
         Assert.assertEquals(expected, response);
     }
-    @Test
+    //DESAFIO!!!!
+//    @Test
+//    public void deveContarImpares(){
+//        Application application = new Application();
+//        Integer expected = 5;
+//        Integer response = application.quantImpar(10);
+//        Assert.assertEquals(expected, response);
+//
+//    }
+
+    //Teste de Strings
+        @Test
     public void deveMostrarLetrasMaiusculas(){
-        Application application = new Application();
         String expected = "RAFAEL";
         String response = application.maiuscula("rafael");
         Assert.assertEquals(expected, response);
     }
+    @Test
+    public void deveMostrarLetrasMinusculas(){
+        String expected = "rafael";
+        String response = application.minuscula("RAFAEL");
+        Assert.assertEquals(expected, response);
+    }
 
+    @Test
+    public void deveMostrarQuantidadeLetras(){
+        int quantidade = application.quantidadeLetra();
+        Assert.assertEquals(7, quantidade);
+    }
 
+    @Test
+    public void deveMostrarQuantidadeLetrasComEspaco(){
+        Integer expected = 8;
+        Integer response = application.quantidadeLetraComEspaco(" DB1START ");
+        Assert.assertEquals(expected, response);
+    }
+    @Test
+    public void deveMostrar4PrimeirasLetrasDoNome(){
+        String expected = "rafa";
+        String response = application.quatroPrimeirasLetrasDoNome("rafael");
+        Assert.assertEquals(expected, response);
+    }
+    @Test
+    public void deveMostrarApos3LetraDoNome(){
+        String expected = "fael";
+        String response = application.retornarLetrasApos3Letra("Rafael");
+        Assert.assertEquals(expected, response);
+    }
+    @Test
+    public void deveMostrar4UltimasLetrasDoNome(){
+        String expected = "rais";
+        String response = application.retorna4UltimasLetras("Rafael Morais");
+        Assert.assertEquals(expected, response);
+    }
+    @Test
+    public void deveTrocarPrimeiroNomePorAluno(){
+        String expected = "Aluno Morais";
+        String response = application.retornaAluno("Rafael Morais");
+        Assert.assertEquals(expected, response);
+    }
+
+    @Test
+    public void deveRetornarSequenciaDeLetrasSeparadas(){
+        String[] expected = new String[]{"banana", "maca", "melancia"};
+        String[] response = application.textoSeparado("banana, maca, melancia");
+        Assert.assertEquals(expected, response);
+
+    }
+    @Test
+    public void deveMostrarQuantasVogaisTem(){
+        Integer expected = 1;
+        Integer response = application.contarVogal("DB1START");
+        Assert.assertEquals(expected, response);
+    }
+
+    @Test
+    public void deveMostrarTextoInvertido(){
+        String expected = "leafar";
+        String response = application.inverterTexto("rafael");
+        Assert.assertEquals(expected, response);
+    }
+
+    // Testes de Matemática
+    @Test
+    public void deveMostrarMenorValor(){
+        Double expected = 13.5;
+        Double response = application.menorValor(13.5, 15.4);
+        Assert.assertEquals(expected, response);
+    }
+    @Test
+    public void deveMostrarOMenorValorEntre3(){
+        Double expected = 9.4;
+        Double response = application.menorValorEntre3(13.4, 14.6, 9.4);
+        Assert.assertEquals(expected, response);
+    }
+    @Test
+    public void deveMostrarAMediaDe3Numeros(){
+        Integer expected = 5;
+        Integer response = application.media(5, 6, 4);
+        Assert.assertEquals(expected, response);
+    }
+    @Test
+    public void deveMostrarAreaDoTriangulo(){
+        Integer expected = 50;
+        Integer response = application.areaDoTriangulo(10, 2);
+        Assert.assertEquals(expected, response);
+    }
 }
--- src/com/db1/db1start/Application.java
+++ src/com/db1/db1start/Application.java
@@ -1,10 +1,15 @@
 package com.db1.db1start;
 
-public class Application {
+import java.util.Arrays;
+
+public class
+Application {
+    //MÉTODOS INTEGER
     public Integer soma(Integer numero1, Integer numero2){
         return numero1 + numero2;
     }
     public Integer multiplicacao(Integer numero1,Integer numero2) {
+
         return numero1 * numero2;
     }
     public Integer divisao(Integer numero1,Integer numero2) {
@@ -31,10 +36,88 @@ public class Application {
         }
         return numImpar;
     }
+    //DESAFIO!!!
+//    public Integer quantImpar (Integer num1){
+//        if(num1 % 2 == 0){
+//            num1 = num1 / 2 ;
+//            return num1;
+//        }
+//        return num1 = (num1 / 2) + 1;
+//    }
 
     //MÉTODOS DE STRINGS
     public String maiuscula(String text){
         return text = text.toUpperCase();
     }
-    
+    public String minuscula(String text){
+        return text = text.toLowerCase();
+    }
+
+    public Integer quantidadeLetra(){
+        String text = "DB1Start";
+        return removeNumeroDoString(text).length();
+
+    }
+    private String removeNumeroDoString(String text){
+        return text.replaceAll("[0-9]", "");
+    }
+
+    public Integer quantidadeLetraComEspaco(String text){
+        String textoSemEspaco = text.replaceAll(" ", "");
+        Integer quantidadeSemEspaco = textoSemEspaco.length();
+        return quantidadeSemEspaco;
+    }
+    public String quatroPrimeirasLetrasDoNome(String nome){
+        return nome = nome.substring(0,4);
+    }
+    public String retornarLetrasApos3Letra(String nome){
+        String letras;
+        return letras = nome.substring(2);
+    }
+    public String retorna4UltimasLetras(String nome){
+        String letras;
+        return letras = nome.substring(9,13);
+    }
+
+    public String retornaAluno(String nome){
+        int posicaoPrimeiroEspaco = nome.indexOf(" ");
+        return "Aluno" + nome.substring(posicaoPrimeiroEspaco);
+
+    }
+
+    public String[] textoSeparado(String texto){
+       return texto.split(", ");
+    }
+
+    public Integer contarVogal(String texto){
+        return texto.trim().replace(" ", "")
+                .toUpperCase()
+                .replaceAll("[^AEIOU]", "")
+                .length();
+    }
+
+    public String inverterTexto(String texto){
+        String textoInvertido = new StringBuilder(texto).reverse().toString();
+        return textoInvertido;
+    }
+
+    //MÉTODOS DE MATEMATICA
+    public Double menorValor(Double valor1, Double valor2){
+        if (valor1 < valor2){
+            return valor1;
+        }
+        return valor2;
+    }
+    public Double menorValorEntre3(Double valor1, Double valor2, Double valor3){
+        return menorValor(menorValor(valor1, valor2),valor3);
+    }
+    public Integer media(Integer num1, Integer num2, Integer num3){
+        num1 = (num1 + num2 + num3) / 3;
+        return num1;
+    }
+    public Integer areaDoTriangulo(Integer base, Integer alt){
+        alt = 2;
+        Integer area = (base*base) / alt;
+        return area;
+    }
 }
